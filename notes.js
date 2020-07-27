65e61724bb42659c23a426dddbed1257cb900669
moti levi <mordilevi11@gmail.com>
2020-07-27 14:57:15 +0300

    notes improve

--- notes.js
+++ notes.js
@@ -3,7 +3,7 @@
  let next_id = 1;
 
  const div = document.getElementById('notes');
- const form = document.getElementById('inputs_field');
+ const form = document.querySelector('.inputs_field');
  form.addEventListener('submit',add_note)
 
 function add_note(e){
@@ -26,29 +26,27 @@ else{
         petek["id"]=next_id;
         Notes.push(petek)
 
-    const image = document.createElement('div'); 
-    const x_button = document.createElement('button')
-    image.classList.add("add_note")
-    image.innerHTML='<img src = "notes_photos/notebg.png" >'
-    div.appendChild(image)
-    x_button.classList.add("delete")
-    x_button.innerHTML = "X";
-    x_button.id= next_id;
-    image.appendChild(x_button);
-    var note_details = document.createElement('div')
-    note_details.innerHTML = petek.text + '<br><br><br>' + petek.date;
-    note_details.classList.add("add_text")
-    image.appendChild(note_details)
+        const card = document.createElement('div');
+        card.classList.add("new_note")
+        card.innerHTML = `<div class = "card_div">
+                           <img src = "notes_photos/notebg.png" class = "add_note">
+                           <button class = "delete" id=${next_id}> X </button>
+                           <span class = "add_text"> ${petek.text}'<br><br><br>'${petek.date} </span>
+                      </div>`
+        div.appendChild(card)
+    
     localStorage.setItem('items', JSON.stringify(Notes))
     localStorage.setItem('counter', JSON.stringify(next_id))
 
-    x_button.onclick = function(){
-        image.remove()
-        Notes = Notes.filter(item=>item.id!==parseInt(x_button.id))
+
+    document.getElementById(next_id).addEventListener('click', function(){
+        this.parentElement.parentElement.remove()
+        Notes = Notes.filter(item=>item.id!==parseInt(this.id))
         localStorage.setItem('items',JSON.stringify(Notes))
 
-    }
 
+    }) 
+        
    next_id++;
 }   
     }
@@ -59,26 +57,23 @@ else{
            next_id = JSON.parse(localStorage.getItem('counter'))+1;
 
            for(var i = 0 ; i < temp.length;i++){
-            const image = document.createElement('div'); 
-            const x_button = document.createElement('button')
-            image.classList.add("add_note")
-            image.innerHTML='<img src = "notes_photos/notebg.png" >'
-            div.appendChild(image)
-            x_button.classList.add("delete")
-            x_button.innerHTML = "X";
-            x_button.id = temp[i].id;
-            image.appendChild(x_button);
-            var note_details = document.createElement('div')
-            note_details.innerHTML = temp[i].text + '<br><br><br>' + temp[i].date ;
-            note_details.classList.add("add_text")
-            image.appendChild(note_details)
 
-            x_button.onclick = function(){
-                image.remove()
-                Notes = Notes.filter(item=>item.id=!x_button.id)
+            const card = document.createElement('div');
+            card.classList.add("new_note")
+            card.innerHTML = `<div class = "card_div">
+                               <img src = "notes_photos/notebg.png" class = "add_note">
+                               <button class = "delete" id=${temp[i].id}> X </button>
+                               <span class = "add_text"> ${temp[i].text}<br><br><br>${temp[i].date} </span>
+                          </div>`
+            div.appendChild(card)
+
+            document.getElementById(temp[i].id).addEventListener('click', function(){
+                this.parentElement.parentElement.remove()
+                Notes = Notes.filter(item=>item.id!==parseInt(this.id))
                 localStorage.setItem('items',JSON.stringify(Notes))
         
-            }
+        
+            }) 
             Notes = temp;
         }
 
